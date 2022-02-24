c6a4cf1c2313e5b74303b65faef93a54a7a54591
Cuong Phan <21520673@gm.uit.edu.vn>
2022-02-24 15:29:14 +0700

    Create main1.cpp

---
+++ Mid-exam/main1.cpp
@@ -0,0 +1,47 @@
+
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+###End banned keyword*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//###INSERT CODE HERE -
+void inputArray(int a[], int &n)
+{
+    cin >> n;
+    for (int i=0;i<n; i++)
+        cin >> a[i];
+}
+int totalFine(int car[], int n, int date, int fine)
+{
+    int sum=0;
+    for (int i=0; i<n; i++)
+    {
+        if (date%2==0 && car[i]%2!=0)
+            sum+=fine;
+        else if (date%2!=0 && car[i]%2==0)
+            sum+=fine;
+    }
+    return sum;
+}
+
+
+
+int main()
+{
+    int car[100],n;
+    inputArray(car,n); // Nhap so luong xe va mang cac bien so xe
+
+    int date; cin>>date; // nhap ngay
+    int fine; cin>>fine; //so tien phat cho moi xe khi vi pham, fine = 250
+
+    //Goi ham tinh tong so tien phat thu ve trong ngay
+    cout << totalFine(car, n, date, fine);
+
+    return 0;
+}

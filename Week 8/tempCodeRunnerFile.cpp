1a53e41b20dd39acb7aa338ab6cf20841688baea
Cuong Phan <21520673@gm.uit.edu.vn>
2022-02-24 15:36:37 +0700

    Create tempCodeRunnerFile.cpp

---
+++ Week 8/tempCodeRunnerFile.cpp
@@ -0,0 +1,40 @@
+using namespace std;
+void sort(int a[],int n)
+{
+    for (int i=0; i<n-1; i++)
+        for (int j=i+1; j<n; j++)
+            if (a[i]>a[j])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+}
+int main()
+{
+    int chan[10000], le[10000];
+    int n,spt_chan=-1,spt_le=-1;
+    cin >> n;
+    for (int i=0; i<n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x % 2 == 0)
+        {
+            spt_chan++;
+            chan[spt_chan]=x;
+        }
+        else
+        {
+            spt_le++;
+            le[spt_le]=x;
+        }
+    }
+    sort(chan,spt_chan);
+    sort(le,spt_le);
+    for (int i=0; i<spt_chan; i++)
+        cout << chan[i] << endl;
+    for (int i=0; i<spt_le; i++)
+        cout << le[i] << endl;
+    return 0;
+}

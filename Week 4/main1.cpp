7249a4f2230e2f58cb8952fbce0bd571f1974dcf
Cuong Phan <21520673@gm.uit.edu.vn>
2022-02-24 15:32:23 +0700

    Create main1.cpp

---
+++ Week 4/main1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n << " " << n << " " << n << " " << n << " " << n << " " << n << endl;
+    cout << n <<"  "<<"  "<<"  "<<"   "<< n << endl;
+    cout << n <<"  "<<"  "<<"  "<<"   "<< n << endl;
+    cout << n << " " << n << " " << n << " " << n << " " << n << " " << n << endl;
+
+    cout << "     " << n <<endl;
+    cout << "    " << n << " " << n   << endl;
+    cout << "   " << n << "   " << n   << endl;
+    cout << "  " << n << "     " << n   << endl;
+    cout << " " << n << "       " << n   << endl;
+    cout << n << " " << n << " " << n << " " << n << " " << n << " " << n << endl;
+    return 0;
+}

13029be6e2ff3e07b43f4c55535a090861d3f60d
Cuong Phan <21520673@gm.uit.edu.vn>
2022-02-24 15:30:14 +0700

    Create main6.cpp

---
+++ Week 2/main6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main()
+{
+    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    double x;
+    cin >> x;
+    cout <<
+
+    
+     setprecision(17)<<  2*x*3.14159265358979323 << "\n" << setprecision(17) << 3.14159265358979323*x*x
+
+
+<< endl;
+    return 0;
+}

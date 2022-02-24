aac99bdc198cb5ce492a5edd32c905148f81d992
Cuong Phan <21520673@gm.uit.edu.vn>
2022-02-24 15:36:17 +0700

    Create main14.cpp

---
+++ Week 8/main14.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+
+using namespace std;
+void sort(int a[],int n)
+{
+    for (int i=1; i<=n-1; i++)
+        for (int j=i+1; j<=n; j++)
+            if (a[i]>a[j])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+}
+int main()
+{
+    int chan[10000], le[10000];
+    int n,spt_chan=0,spt_le=0;
+    cin >> n;
+    for (int i=1; i<=n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x % 2 == 0)
+        {
+            spt_chan++;
+            chan[spt_chan]=x;
+        }
+        else
+        {
+            spt_le++;
+            le[spt_le]=x;
+        }
+    }
+    sort(chan,spt_chan);
+    sort(le,spt_le);
+    for (int i=1; i<=spt_chan; i++)
+        cout << chan[i] << endl;
+    for (int i=1; i<=spt_le; i++)
+        cout << le[i] << endl;
+    return 0;
+}

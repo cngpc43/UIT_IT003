563e9890a6eb2f4d1d952be66f011eb30b77cb71
Cuong Phan <21520673@gm.uit.edu.vn>
2022-02-24 15:39:17 +0700

    Create main6.cpp

---
+++ Week 12.1/main6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void replaceChar(char s1[], char s2[], char ch)
+{
+    for (int i=0; i<strlen(s1); i++)
+        for (int j=0; j<strlen(s2); j++)
+            if (s1[i]==s2[j])
+                s1[i]=ch;
+       
+   
+}
+
+
+
+int main()
+{
+
+   char s1[100],s2[100];
+   cin.getline(s1,100);
+   cin.getline(s2,100);
+   // Thay nhung ky tu trong s1 ma co chua trong s2 thanh '*'
+   replaceChar(s1, s2, '*');
+   cout<<s1;
+
+   return 0;
+}

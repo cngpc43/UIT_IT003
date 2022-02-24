0bd2ee3201072151a96e521d6dcc79792f38d3ff
Cuong Phan <21520673@gm.uit.edu.vn>
2022-02-24 15:30:06 +0700

    Create main10.cpp

---
+++ Week 2/main10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    double x;
+    cin >> x;
+    cout <<
+
+        (x-32)*5/9 << " " << (x-32)*5/9+273.15
+
+     << endl;
+    return 0;
+}
